5f2f8e9225598b6c844f33fc1ee8c4da0ecee4e2
timothy.credo@ibm.com <timothy.credo@ibm.com>
2021-10-28 20:55:34 +0200

    feat: endpoint for getting all facts from db

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
-import requests
 import psycopg2
+import requests
+from fastapi import FastAPI
+from psycopg2.extras import RealDictCursor
 
 
 app = FastAPI()
+
+
+db_config = {
+    'database': 'postgres',
+    'user': 'postgres',
+    'password': 'woof',
+    'host': 'dog-facts-db',
+    'port': '5432',
+}
+
 
 @app.get("/")
 async def get_fact():
@@ -13,10 +24,21 @@ async def get_fact():
     
     return {"fact": response}
 
+
 @app.get("/check_db")
 async def check_db():
     try:
-        con = psycopg2.connect(database="dog-facts-db", user="postgres", password="woof", host="dog-facts-db", port="5432")
-        print("Database opened successfully")
+        con = psycopg2.connect(**db_config)
+        return "Database opened successfully"
     except Exception as e:
-        print(repr(e))
+        return repr(e)
+
+
+@app.get("/get_all_facts")
+async def get_all_facts():
+    conn = psycopg2.connect(**db_config)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    query_sql = "SELECT id, fact FROM dog_facts" 
+    cur.execute(query_sql)
+    results = cur.fetchall()
+    return results
